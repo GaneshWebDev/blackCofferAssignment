4be82505115d3e03c5b5c28510ea8942ecf73366
20215ganesh <muzinoasahi@gmail.com>
2024-03-01 15:39:28 +0530

    third commit

--- src/components/explore.tsx
+++ src/components/explore.tsx
@@ -1,16 +1,17 @@
-import Personal from "./personal"
+/*import Personal from "./personal"
 import Business from "./Business"
 import Merchant from "./merchant"
+import { RefObject } from "react"
 interface ExploreProps{
     header:number,
     search:number,
-    personal:HTMLDivElement,
-    business:HTMLDivElement,
-    merchant:HTMLDivElement,
+    personal:RefObject<HTMLDivElement>,
+    business:RefObject<HTMLDivElement>,
+    merchant:RefObject<HTMLDivElement>,
     selectedIcon:string
 }
 export default function Explore({header,search,personal,business,merchant,selectedIcon}:ExploreProps){
-    console.log(selectedIcon,'explore')
+    console.log(selectedIcon,)
     return(
         <div className="grid grid-flow-col grid-cols-[100vw] overflow-x-auto overscroll-x-contain snap-x snap-mandatory w-screen  z-0 scroll-smooth" id='explore'>
             <div className="snap-center w-screen" ref={personal}>
@@ -26,5 +27,35 @@ export default function Explore({header,search,personal,business,merchant,select
         
          
 
+    )
+}*/
+import Personal from "./personal"
+import Business from "./Business"
+import Merchant from "./merchant"
+import { RefObject } from "react"
+
+interface ExploreProps {
+    header: number,
+    search: number,
+    personal: RefObject<HTMLDivElement>,
+    business: RefObject<HTMLDivElement>,
+    merchant: RefObject<HTMLDivElement>,
+    selectedIcon: string
+}
+
+export default function Explore({ header, search, personal, business, merchant, selectedIcon }: ExploreProps) {
+    console.log(selectedIcon)
+    return (
+        <div className="grid grid-flow-col grid-cols-[100vw] overflow-x-auto overscroll-x-contain snap-x snap-mandatory w-screen z-0 scroll-smooth" id='explore'>
+            <div className="snap-center w-screen" ref={personal}>
+                <Personal header={header} search={search} selectedIcon={selectedIcon} />
+            </div>
+            <div className="snap-center w-screen" ref={business}>
+                <Business header={header} search={search} selectedIcon={selectedIcon} />
+            </div>
+            <div className="snap-center w-screen" ref={merchant}>
+                <Merchant header={header} search={search} selectedIcon={selectedIcon} />
+            </div>
+        </div>
     )
 }
