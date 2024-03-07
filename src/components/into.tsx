695d72e709fb29969e2ec67e24cf0d9f223e3a96
Ganesh_web_dev <115787522+GaneshWebDev@users.noreply.github.com>
2024-03-07 18:50:52 +0530

    Update into.tsx

--- src/components/into.tsx
+++ src/components/into.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 export default function Intro(){
     return(
         <section className="bg-center bg-no-repeat bg-[url('https://flowbite.s3.amazonaws.com/docs/jumbotron/conference.jpg')] bg-gray-700 bg-blend-multiply">
@@ -5,12 +6,14 @@ export default function Intro(){
                 <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-white md:text-5xl lg:text-6xl">Welcome To Netclan</h1>
                 <p className="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Connecting the Unconnected around you!</p>
                 <div className="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0">
-                    <a href="/main" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" >
+                     <Link to='/main'>
+                    <span href="/main" className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-900" >
                         Get started
                         <svg className="w-3.5 h-3.5 ms-2 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
                             <path stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
                         </svg>
-                    </a>
+                    </span>
+                     </Link>
                 </div>
             </div>
         </section>
